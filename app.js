4f631c38f9ad1739e59320a36154ccc79877358f
catop98 <46827761+catop98@users.noreply.github.com>
2021-02-05 02:30:15 -0500

    Version 1

---
+++ app.js
@@ -0,0 +1,44 @@
+(function(){
+	function filePreview(input){
+		if (input.files && input.files[0]){
+			var reader = new FileReader();
+
+			reader.onload = function(e){
+				$('#imagePreview').html("<img src='"+e.target.result+"' />");
+			}
+
+			reader.readAsDataURL(input.files[0]);
+		}
+	}
+
+	$('#imagedoc').change(function(el){
+  if(LimitAttach(this,1))
+		  filePreview(this);
+	});
+})();
+
+
+function LimitAttach(tField,iType) {
+	file=tField.value;
+	if (iType==1) {
+	extArray = new Array(".jpg");
+	}	
+	allowSubmit = false;
+	if (!file) return false;
+	while (file.indexOf("\\") != -1) file = file.slice(file.indexOf("\\") + 1);
+  ext = file.slice(file.indexOf(".")).toLowerCase();
+  for (var i = 0; i < extArray.length; i++) {
+    if (extArray[i] == ext) {
+      allowSubmit = true;
+      break;
+	}
+	}
+	if (allowSubmit) {
+  return true
+	} else {
+	tField.value="";
+	alert("Usted sólo puede subir archivos con extensiones " + (extArray.join("JPG ")) + "\n Reiniciando Formulario");
+return false;
+	setTimeout("location.reload()",2000);
+	}
+	}	
